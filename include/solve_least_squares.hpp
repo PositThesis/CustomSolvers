aa792b422536d1201f4fed6d205a8d0afd671222
Yannik Böttcher <yannikboettcher@outlook.de>
2022-10-14 16:43:23 +0200

    parallelize residual computation, make skipping prettier

--- include/solve_least_squares.hpp
+++ include/solve_least_squares.hpp
@@ -14,7 +14,6 @@ template <typename Scalar>
 VectorX<Scalar> solve_least_squares(QRDecompositionResult<Scalar> &qr, Scalar rho_0, Eigen::Index size) {
     MatrixX<Scalar> R = qr.R.block(0, 0, size, size);
     VectorX<Scalar> q = rho_0 * qr.Q.transpose().block(0, 0, size, 1);
-
     VectorX<Scalar> z = VectorX<Scalar>::Zero(q.rows());
 
     // solve via forward substitution
@@ -39,31 +38,25 @@ VectorX<Scalar> solve_least_squares(QRDecompositionResult<Scalar> &qr, Scalar rh
 template <typename Scalar, typename MatrixType>
 std::vector<Scalar> solve_all_least_squares(MatrixX<Scalar> &H, MatrixX<Scalar> &V, MatrixType &A, VectorX<Scalar> rhs, VectorX<Scalar> x_0, Scalar rho_0) {
     std::vector<Scalar> residuals;
+    for (Eigen::Index iter = 0; iter < H.cols(); iter++) {
+        residuals.push_back(-1000);
+    }
     QRDecompositionResult<Scalar> qr = qr_decompose_hessenberg(H);
 
+#pragma omp parallel for
     for (Eigen::Index iter = 0; iter < H.cols(); iter++) {
-        VectorX<Scalar> z = solve_least_squares<Scalar>(qr, rho_0, iter+1);
-        VectorX<Scalar> x = x_0 + V.block(0, 0, V.rows(), iter+1) * z;
+        // compute all below 500, every fifth below 1000, every tenth below 2000 and every 20th afterwards
+        if (iter < 500 || (iter < 1000 && (iter % 5) == 0) || (iter < 2000 && (iter % 10) == 0) || ((iter % 20) == 0)) {
+            VectorX<Scalar> z = solve_least_squares<Scalar>(qr, rho_0, iter+1);
+            VectorX<Scalar> x = x_0 + V.block(0, 0, V.rows(), iter+1) * z;
 
-        VectorX<Scalar> error = (rhs - A * x);
-        Scalar res_squared = error.transpose() * error;
-        Scalar residual = std::sqrt(res_squared);
-        residuals.push_back(residual);
+            VectorX<Scalar> error = (rhs - A * x);
+            Scalar res_squared = error.transpose() * error;
+            Scalar residual = std::sqrt(res_squared);
+#pragma omp critical
+            residuals[iter] = residual;
+        } 
         if (iter % 10 == 0) {
-            std::cout << "residual " << iter << " / " << H.cols() << " done" << std::endl;
-        }
-        if (iter > 1000) {
-            // skip 4 values
-            for (int j = 0; j < 4 && ++iter < H.cols(); j++) {
-                residuals.push_back(-1000);
-            }
-            std::cout << "residual " << iter << " / " << H.cols() << " done" << std::endl;
-        }
-        if (iter > 2000) {
-            // skip 5 more values
-            for (int j = 0; j < 5 && ++iter < H.cols(); j++) {
-                residuals.push_back(-1000);
-            }
             std::cout << "residual " << iter << " / " << H.cols() << " done" << std::endl;
         }
     }
